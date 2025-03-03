f3eb7f98ca6ccd587ef372141add697aae145f31
Gabriel Restrepo <gabrielrestrepo7@gmail.com>
2025-03-02 20:43:52 -0500

    add security to all projects

--- Microservicios/microservicio-notificacion/src/main/java/co/analisys/biblioteca/controller/NotificacionController.java
+++ Microservicios/microservicio-notificacion/src/main/java/co/analisys/biblioteca/controller/NotificacionController.java
@@ -4,6 +4,7 @@ import co.analisys.biblioteca.dto.NotificacionDTO;
 import co.analisys.biblioteca.service.NotificacionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @RestController
 @RequestMapping("/notificar")
@@ -12,6 +13,7 @@ public class NotificacionController {
     private NotificacionService notificacionService;
 
     @PostMapping
+    @PreAuthorize("hasRole('ROLE_NOTIFIER', 'ROLE_LIBRARIAN')")
     public void enviarNotificacion(@RequestBody NotificacionDTO notificacion) {
         notificacionService.enviarNotificacion(notificacion);
     }
